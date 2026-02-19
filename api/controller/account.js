aca1a43c5fa10bedeaef7b2e1ff50cf098cfca1f
shyamdigitalit <donotreply.digital@shyamgroup.com>
2026-02-19 11:45:31 +0530

    Account api dev done

--- api/controller/account.js
+++ api/controller/account.js
@@ -1,52 +1,52 @@
 import Account from "../model/account.js";
 
 const createAccount = async (req, res) => {
-    try {
-        const {accountName, accountEmail, phoneNumber, employeeCode, location, dateOfBirth, anniversaryDate, status } = req.body;
+  try {
+    const {accountName, accountEmail, phoneNumber, employeeCode, location, status } = req.body;
 
-        if (!accountName || !accountEmail) {
-            return res.status(400).json({ status: 400, message: "Account name and email are required" });
-        }
-
-        const existingAccount = await Account.findOne({ accountEmail });
-        if (existingAccount) {
-            return res.status(400).json({ status: 400, message: "Account with this email already exists" });
-        }
-
-        const result = await Account.create({ accountName, accountEmail, phoneNumber, employeeCode, location, dateOfBirth, anniversaryDate, status });
-
-        res.status(200).json({ status: 200, message: "Account created successfully", data: result });
-    } catch (error) {
-        res.status(500).json({ status: 500, message: "Error creating account", error: error.message });
+    if (!accountName || !accountEmail) {
+        return res.status(400).json({ status: 400, message: "Account name and email are required" });
     }
+
+    const existingAccount = await Account.findOne({ accountEmail });
+    if (existingAccount) {
+        return res.status(400).json({ status: 400, message: "Account with this email already exists" });
+    }
+
+    const result = await Account.create({ accountName, accountEmail, phoneNumber, employeeCode, location, status });
+
+    res.status(200).json({ status: 200, message: "Account created successfully", data: result });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Error creating account", error: error.message });
+  }
 };
 
 const bulkCreateAccount = async (req, res) => {
-    try {
-        const { data } = req.body;
-        const result = await Account.insertMany(data);
-        console.log(result)
-        res.status(200).json({ status: 200, message: "Account created successfully", data: result });
-    } catch (error) {
-        res.status(500).json({ status: 500, message: "Error creating account", error: error.message });
-    }
+  try {
+    const { data } = req.body;
+    const result = await Account.insertMany(data);
+    // console.log(result)
+    res.status(200).json({ status: 200, message: "Account created successfully", data: result });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Error creating account", error: error.message });
+  }
 };
 
 
 const updateAccount = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const updatedAccount = await Account.findByIdAndUpdate( id, req.body, { new: true, runValidators: true } );
+    const updatedAccount = await Account.findByIdAndUpdate( id, req.body, { new: true, runValidators: true } );
 
-        if (!updatedAccount) {
-            return res.status(404).json({ status: 404, message: "Account not found" });
-        }
-
-        res.status(200).json({ status: 200, message: "Account updated successfully", data: updatedAccount });
-    } catch (error) {
-        res.status(500).json({ status: 500, message: "Error updating account", error: error.message });
+    if (!updatedAccount) {
+        return res.status(404).json({ status: 404, message: "Account not found" });
     }
+
+    res.status(200).json({ status: 200, message: "Account updated successfully", data: updatedAccount });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Error updating account", error: error.message });
+  }
 };
 
 
@@ -73,63 +73,10 @@ const accountList = async (req, res) => {
 
     const pipeline = [
       { $match: matchStage },
-      { $addFields: { birthMonth: { $month: "$dateOfBirth" }, birthDay: { $dayOfMonth: "$dateOfBirth" }, }, },
-
-      {
-        $addFields: {
-          birthdayThisYear: {
-            $dateFromParts: {
-              year: { $year: today },
-              month: "$birthMonth",
-              day: "$birthDay",
-            },
-          },
-        },
-      },
-
-      {
-        $addFields: {
-          nextBirthday: {
-            $cond: [
-              { $lt: ["$birthdayThisYear", today] },
-              {
-                $dateFromParts: {
-                  year: { $add: [{ $year: today }, 1] },
-                  month: "$birthMonth",
-                  day: "$birthDay",
-                },
-              },
-              "$birthdayThisYear",
-            ],
-          },
-        },
-      },
-
-      {
-        $addFields: {
-          daysLeftForBirthday: {
-            $ceil: {
-              $divide: [
-                { $subtract: ["$nextBirthday", today] },
-                1000 * 60 * 60 * 24,
-              ],
-            },
-          },
-        },
-      },
 
       { $sort: { updatedAt: -1 } },
       { $skip: skip },
       { $limit: limit },
-      {
-        $project: {
-          birthMonth: 0,
-          birthDay: 0,
-          birthdayThisYear: 0,
-          nextBirthday: 0,
-          __v: 0,
-        },
-      },
     ]
 
     const [accounts, total] = await Promise.all([
@@ -154,13 +101,13 @@ const accountList = async (req, res) => {
 
 
 const deleteAccount = async (req, res) => {
-    try {
-        const selectedIds = req.body; // Expecting an array of IDs to delete
-        const deletedAccount = await Account.deleteMany({ _id: { $in: selectedIds } });
-        res.status(200).json({ status: 200, message: "Accounts deleted successfully", data: deletedAccount });
-    } catch (error) {
-        res.status(500).json({ status: 500, message: "Error deleting activities", error: error.message });
-    }
+  try {
+    const selectedIds = req.body; // Expecting an array of IDs to delete
+    const deletedAccount = await Account.deleteMany({ _id: { $in: selectedIds } });
+    res.status(200).json({ status: 200, message: "Accounts deleted successfully", data: deletedAccount });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Error deleting activities", error: error.message });
+  }
 }   
 
 
--- api/model/account.js
+++ api/model/account.js
@@ -4,11 +4,9 @@ const accountSchema = new mongoose.Schema(
     {
         accountName: { type: String, required: true, trim: true },
         accountEmail: { type: String, required: true, unique: true, lowercase: true, trim: true },
-        dateOfBirth: { type: Date, required: true },
         phoneNumber: { type: String, trim: true },
         employeeCode: { type: String, trim: true },
         location: { type: String, trim: true },
-        anniversaryDate: { type: Date },
         status: { type: String, required: true, enum: ['Active', 'Inactive'], default: 'Active' },
     },
     { timestamps: true  }
